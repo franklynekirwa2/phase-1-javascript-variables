32db486100903a951ccf63dfebeb5687a326e2f9
franklynekirwa2 <sangkirwa2@gmail.com>
2024-06-25 12:19:12 +0300

    javascript-variables

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution in this file!
+
+const companyName='Scuber';
+let mostProfitableNeighborhood='Chelsea';
+let companyCeo='Susan Smith';
+expect(mostProfitableNeighborhood).to.equal('Chelsea');
+expect(companyCeo).to.equal('Susan Smith');
+
